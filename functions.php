90fd93d40ca79b376d7870b561383b0166af5864
contact@duocnguyen.me <contact@duocnguyen.me>
2014-09-30 21:49:55 +0700

    init

---
+++ functions.php
@@ -0,0 +1,44 @@
+<?php
+add_filter('img_caption_shortcode', 'sv_img_caption_shortcode', 10, 3);
+function sv_img_caption_shortcode($output ,$atts, $content = null)
+{
+    if ($atts['width'] < 1 || empty($atts['caption']))
+        return $content;
+
+    if (!empty($atts['id']))
+        $atts['id'] = 'id="' . esc_attr($atts['id']) . '" ';
+    $id = explode('_', $atts['id'])[1];
+    $meta = get_post( $id );
+    $description = ($meta->post_content?'<span class="description">' . $meta->post_content . '</span>':'');
+    $class = trim($atts['align'] . ' ' . $atts['class'] . ' ' . 'sv_container hover-style-1');
+    if (current_theme_supports('html5', 'caption')) {
+        return '<figure ' . $atts['id'] . 'style="width: ' . (int)$atts['width'] . 'px;" class="' . esc_attr($class) . '">'
+        . do_shortcode($content) . '<figcaption class="figcaption"><span class="caption">' . $atts['caption'] . '</span>' . $description . '</figcaption></figure>';
+    }
+
+    $caption_width =$atts['width'];
+
+    /**
+     * Filter the width of an image's caption.
+     *
+     * By default, the caption is 10 pixels greater than the width of the image,
+     * to prevent post content from running up against a floated image.
+     *
+     * @since 3.7.0
+     *
+     * @see img_caption_shortcode()
+     *
+     * @param int $caption_width Width of the caption in pixels. To remove this inline style,
+     *                              return zero.
+     * @param array $atts Attributes of the caption shortcode.
+     * @param string $content The image element, possibly wrapped in a hyperlink.
+     */
+    $caption_width = apply_filters('img_caption_shortcode_width', $caption_width, $atts, $content);
+
+    $style = '';
+    if ($caption_width)
+        $style = 'style="width: ' . (int)$caption_width . 'px" ';
+
+    return '<div ' . $atts['id'] . $style . 'class="' . esc_attr($class) . '">'
+    . do_shortcode($content) . '<div class="figcaption"><span class="caption">' . $atts['caption'] . '</span>' . $description . '</div></div>';
+}
